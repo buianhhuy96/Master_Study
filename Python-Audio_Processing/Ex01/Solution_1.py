ceb1732b266558d94a3b75a5fb1f72ae5b6e94c9
Bui Anh Huy <huy.bui-anh@internal.banvien.com>
2020-10-29 18:07:14 +0700

    Audio Processing - Ex01 - Update

--- Python-Audio_Processing/Ex01/Solution_1.py
+++ Python-Audio_Processing/Ex01/Solution_1.py
@@ -1,31 +1,57 @@
+#!/usr/bin/python
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.io import wavfile
 from scipy.fftpack import fft
-#define omega(f) (2 * np.pi * f)
 
-Fs = 8000
-f100 = 100
-f500 = 500
-f1500 = 1500
-f2500 = 2500
-SimulationTime = 3
-NumOfSample = int(SimulationTime*Fs);
-x = np.linspace(0, SimulationTime,NumOfSample)
-y1 = np.sin(2 * np.pi * f100 * x +1)
-y2 = 2*np.sin(2 * np.pi * f500 * x +2)
-y3 = 3*np.sin(2 * np.pi * f1500 * x +3)
-y4 = 1.5*np.sin(2 * np.pi * f2500 * x +4)
-y_sum=y1+y2+y3+y4
-wavfile.write('Sine.wav', Fs, y_sum)
-
-yf = np.fft.fftshift(fft(y_sum,512))
-xf = np.linspace(int(-Fs/(2.0)), int(Fs/(2.0)), 512)#int(2*Fs ))
-fig, axs = plt.subplots(2)
-
-axs[0].plot(xf, 2/512 * np.abs(yf))#[int(NumOfSample/2-Fs):int(NumOfSample/2+Fs)]))
-#axs[1].plot(xf, np.abs(yf)/NumOfSample)
-#axs[1].xlabel('sample(n)')
-#axs[1].ylabel('voltage(V)')
-#axs[1].axis('tight')
-plt.show()
+def main():
+    # - Define sampling parameters
+    # - Highest frequency in this exercise is 2500 so 
+    # Fs must be at least 5000 (Nyquist Theorem), I chose 10000 
+    # to preserve waveform 
+    Fs = 10000
+    SimulationTime = 3
+    NumOfSample = int(SimulationTime*Fs)
+    
+    # Define 4 sine signal with given frequency
+    # and different chosen amplitudes and phases
+    t = np.linspace(0, SimulationTime,NumOfSample)
+    x = [ 3*np.sin(2 * np.pi * 100 * t +1), 
+          1.5*np.sin(2 * np.pi * 500 * t +2),
+          np.sin(2 * np.pi * 1500 * t +3),
+          2*np.sin(2 * np.pi * 2500 * t +4)]
+    
+   
+    x_sum = 0
+    for i in range(0,len(x)):
+        x_sum += x[i]
+        
+    wavfile.write('Sine.wav', Fs, x_sum)
+    
+    # Define DFT-point 
+    N_point = 512
+    
+    # Fast Fourier Transform summed signal to get output in frequency field 
+    # fftshift to plot both negative and positive part
+    ff = np.linspace(0, int(Fs), N_point)
+    xf = fft(x_sum,N_point)
+    
+    plot_range = int(Fs/300)     
+    plt.figure(1)
+    for i in range(0,len(x)):
+        plt.plot(t[0:plot_range],x[i][0:plot_range], label = 'x_' + str(i) + '(t)')
+    plt.legend()
+    plt.figure(2)
+    plt.plot(t[0:plot_range],x_sum[0:plot_range])
+    
+    
+    plt.figure(3)    
+    plt.plot(ff[0: int(N_point/3)], 2/N_point * np.abs(xf)[0: int(N_point/3)], label ='X_f')
+    plt.title('DFT - Spectral density of x_sum(t)')
+    plt.xlabel('Frequency (Hz)')
+    plt.ylabel('Magnitude')
+    plt.legend()
+    plt.show()
+    
+if __name__== "__main__":
+    main()
