5a0ea59c25869b3f03f68d8b0dd0b4de779fd8b1
Bui Anh Huy <huy.bui-anh@internal.banvien.com>
2021-01-05 17:45:22 +0700

    Update OpenCV projects

---
+++ Projects/OpenCV/Pose_Estimation/Project_OpenCV/Project_OpenCV.cpp
@@ -0,0 +1,188 @@
+﻿// C++
+#include <iostream>
+#include <time.h>
+// OpenCV
+#include <opencv2/core.hpp>
+#include <opencv2/core/utility.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/calib3d.hpp>
+#include <opencv2/video/tracking.hpp>
+// PnP Tutorial
+#include "Mesh.h"
+#include "Model.h"
+#include "PnPProblem.h"
+#include "ModelRegistration.h"
+#include "Utils.h"
+
+/**  GLOBAL VARIABLES  **/
+
+using namespace cv;
+using namespace std;
+
+string yml_read_path = "Data/mybox.yml";			// 3D Keypoints + Descriptors
+string ply_read_path = "Data/mybox.ply";			// Mesh
+
+/*define*/
+Scalar red(0, 0, 255);
+Scalar green(0, 255, 0);
+Scalar blue(255, 0, 0);
+Scalar yellow(0, 255, 255);
+
+// Intrinsic camera parameters
+double f = 5;                           // focal length in mm
+double sx = 6.17, sy = 4.56;             // sensor size
+double width = 640, height = 480;        // image size
+double params_camera[] = { width*f / sx, height*f / sy, width / 2, height / 2 };   // fx,fy,cx,cy
+PnPProblem pnp_detection(params_camera);
+
+// Model variables
+Mesh mesh;                 
+Model model;
+vector<Point3f> list_points3d_model;
+Mat descriptors_model;
+
+// Scene variables
+vector<KeyPoint> keypoints_scene;	// to obtain the 2D points of the scene
+Mat descriptors_scene;
+
+// Matcher variables
+vector<vector<DMatch>> matches;
+vector<DMatch>	 good_matches;
+vector<Point3f> list_points3d_model_match; 
+vector<Point2f> list_points2d_scene_match; 
+
+// PnP RANSAC variables
+Mat inliers_idx;
+vector<Point2f> list_points2d_inliers;
+
+// Detector parameters
+int numKeyPoints = 5000;      // number of detected keypoints
+Ptr<FeatureDetector> detector = ORB::create(numKeyPoints);
+
+// Matcher parameters
+Ptr<flann::IndexParams> indexParams = makePtr<flann::LshIndexParams>(6, 12, 1); // instantiate LSH index parameters
+Ptr<flann::SearchParams> searchParams = makePtr<flann::SearchParams>(50);       // instantiate flann search parameters																	
+Ptr<DescriptorMatcher> matcher = makePtr<FlannBasedMatcher>(indexParams, searchParams);// instantiate FlannBased matcher
+int k = 4;
+float ratioTest = 0.70f;      // ratio test
+int good_threshold = 15; 
+bool good_measurement = false;
+
+// PnP RANSAC parameters
+int pnpMethod = SOLVEPNP_ITERATIVE;
+int iterationsCount = 100;      // number of Ransac iterations.
+float reprojectionError = 3.0;  // maximum allowed distance to consider it an inlier.
+double confidence = 0.95;        // ransac successful confidence.
+
+/*Function header*/
+
+int main(int argc, char *argv[])
+{
+/** Step 0: Load Model and Mesh **/
+	mesh.load(ply_read_path);  
+	model.load(yml_read_path);
+	list_points3d_model = model.get_points3d();
+	descriptors_model = model.get_descriptors();
+
+	namedWindow("Object Detection", WINDOW_KEEPRATIO);
+	VideoCapture cap;
+	cap.open(0);
+	Mat frame, frame_vis;
+
+	while (cap.read(frame) && (char)waitKey(30) != 27)
+	{
+		frame_vis = frame.clone();
+		matches.clear();
+		good_matches.clear();
+
+		/*Scene Keypoints Detection & Descriptors Extraction*/
+		detector->detect(frame, keypoints_scene);
+		detector->compute(frame, keypoints_scene, descriptors_scene);
+
+		/* Matching descriptors between model and scene*/
+		matcher->knnMatch(descriptors_scene, descriptors_model, matches, k);
+		for (vector<vector<DMatch> >::iterator matchIterator = matches.begin(); matchIterator != matches.end(); ++matchIterator)
+		{
+			// if 2 NN has been identified
+			if (matchIterator->size() > 1)
+			{
+				// check distance ratio
+				if ((*matchIterator)[0].distance / (*matchIterator)[1].distance > ratioTest)
+				{
+					matchIterator->clear(); // remove match
+				}
+			}
+			else
+			{ // does not have 2 neighbours
+				matchIterator->clear(); // remove match
+			}
+		}
+		for (vector<vector<DMatch>>::iterator matchIterator = matches.begin(); matchIterator != matches.end(); ++matchIterator)
+		{
+			if (!matchIterator->empty()) good_matches.push_back((*matchIterator)[0]);
+		}
+
+		if (good_matches.size() > 10)  /*RANSAC crash if good matches = 0*/
+		{
+
+			/**Step 2: Find out the 2D/3D correspondences**/
+			list_points3d_model_match.clear();
+			list_points2d_scene_match.clear();
+			for (unsigned int match_index = 0; match_index < good_matches.size(); ++match_index)
+			{
+				Point3f point3d_model = list_points3d_model[good_matches[match_index].trainIdx];	// 3D point from model
+				Point2f point2d_scene = keypoints_scene[good_matches[match_index].queryIdx].pt;		// 2D point from the scene
+				list_points3d_model_match.push_back(point3d_model);         // add 3D point
+				list_points2d_scene_match.push_back(point2d_scene);         // add 2D point
+			}
+
+			/** Step 3: Estimate the pose using RANSAC approach **/
+			pnp_detection.estimatePoseRANSAC(list_points3d_model_match, list_points2d_scene_match, pnpMethod, inliers_idx, iterationsCount, reprojectionError, confidence);
+			
+			/** Step 4: Catch the inliers keypoints to draw **/
+			for (int inliers_index = 0; inliers_index < inliers_idx.rows; ++inliers_index)
+			{
+				int n = inliers_idx.at<int>(inliers_index);         // i-inlier
+				circle(frame_vis, list_points2d_scene_match[n], 3, green, 1);
+			}
+
+			/** Step 5: Check measurement**/
+			/*Check measurement*/
+			if (inliers_idx.rows >= good_threshold)
+			{
+				good_measurement = true;
+				/*Get the measured translation & measured rotation*/
+				Mat translation_measured(3, 1, CV_64F);
+				Mat rotation_measured(3, 3, CV_64F);
+				//translation_measured = pnp_detection.get_t_matrix();
+				//rotation_measured = pnp_detection.get_R_matrix();
+			}
+
+			/** Step 7: Draw pose **/
+			double l = 5;
+			vector<Point2f> pose_points2d;
+			if (good_measurement)
+			{
+				good_measurement = false;
+				drawObjectMesh(frame_vis, &mesh, &pnp_detection, blue);  // draw pose estimated by RANSAC
+				pose_points2d.push_back(pnp_detection.backproject3DPoint(Point3f(0, 0, 0)));  // axis center
+				pose_points2d.push_back(pnp_detection.backproject3DPoint(Point3f(l, 0, 0)));  // axis x
+				pose_points2d.push_back(pnp_detection.backproject3DPoint(Point3f(0, l, 0)));  // axis y
+				pose_points2d.push_back(pnp_detection.backproject3DPoint(Point3f(0, 0, l)));  // axis z
+				draw3DCoordinateAxes(frame_vis, pose_points2d);
+			}
+
+			/** Step X: show info **/
+			string text = "Inliers: " + IntToString(inliers_idx.rows) + " of " + IntToString((int)good_matches.size()) + " matches" ;
+			drawText(frame_vis, text, green);
+			imshow("Object Detection", frame_vis);
+		}
+		else	imshow("Object Detection", frame);
+	}
+
+	destroyWindow("Object Detection");
+	cout << "GOODBYE ..." << endl;
+}
+
+
---
+++ Projects/OpenCV/XOboard/XOboard/main.cpp
@@ -0,0 +1,352 @@
+#include "stdafx.h"
+#include "opencv2/core/core.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/video/video.hpp"
+#include "opencv2/features2d/features2d.hpp"
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
+#include "opencv2/ml/ml.hpp"
+#include <vector>
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+using namespace std;
+using namespace cv;
+
+Mat src, src_g, src_b;
+Mat M, board, board_g, board_b;
+Mat draw, drawX, drawO;
+int Threshold = 128;
+int board_rows = 0, board_cols = 0;
+int square_count = 0;
+float square_w, square_h;
+int level1 = 10, level2 = 150;
+int setup_flag = 0, flag = -1;
+int calibLine = 4;
+int sum_x = 0, sum_y = 0;
+vector<vector<Point>> contours;
+vector<Point> border;
+vector<Point> vertices;
+vector<vector<Point>> squares(200, vector<Point>(4));
+void SetupBackground();
+void DetectBorder();
+void DetectSquares();
+void SetupDraw();
+void redrawBoard();
+void getBoard();
+void processSquare(vector<Point> square_corners);
+int Diagonal(vector<Point> rect);
+int findXO(Mat img);
+int findXO2(Mat img);
+float findblack(Mat img);
+
+int main()
+{
+	VideoCapture cap;
+	cap.open(0);
+	if (!cap.isOpened())  // Connect to camera
+	{
+		cout << "Cannot open the webcam" << endl;					
+		return -1;
+	}
+	bool bSuccess = cap.read(src); // Read a frame
+	if (!bSuccess)
+	{
+		cout << "Cannot read a frame from video stream" << endl;	
+		return -1;
+	}
+	//src = imread("xoil.jpg");
+	SetupBackground();
+	DetectBorder();
+	DetectSquares();
+	SetupDraw();
+	while (waitKey(50) != 'q')
+	{	
+		bool bSuccess = cap.read(src); // Read a frame
+		if (!bSuccess)
+		{
+			cout << "Cannot read a frame from video stream" << endl;	
+			return -1;
+		}
+		getBoard();
+		redrawBoard();
+		for (size_t i = 0; i < square_count; i++)
+		{
+			try
+			{
+				processSquare(squares[i]);
+			}
+			catch (int e)
+			{
+
+			}
+		}
+		imshow("Draw", draw);
+	}
+	return 0;
+}
+
+/* Adjust threshold to get clear XO board */
+void SetupBackground()
+{
+	namedWindow("SetupBackground", WINDOW_NORMAL);
+	createTrackbar("Threshold", "SetupBackground", &Threshold, 255);
+	while (waitKey(20) != 13) // If OK -> Enter
+	{
+		cvtColor(src, src_g, CV_BGR2GRAY);
+		medianBlur(src_g, src_g, 1);
+		threshold(src_g, src_b, Threshold, 255, THRESH_BINARY);
+
+		imshow("SetupBackground", src_b);
+	}
+	destroyWindow("SetupBackground");
+}
+
+/* Detect board borders, cut and stretch them out */
+void DetectBorder()
+{
+	int diag, diag_max = 0;
+	int diag_src = sqrt(src.cols*src.cols + src.rows*src.rows);
+	findContours(src_b, contours, RETR_LIST, CHAIN_APPROX_SIMPLE);
+	for (size_t i = 0; i < contours.size(); i++)
+	{
+		approxPolyDP(contours[i], vertices, arcLength(contours[i], 1)*0.02, 1);
+		/* Check if picking a quadrilateral */
+		if (vertices.size() == 4)
+		{
+			diag = Diagonal(vertices);
+			/* Pick the largest quadrilateral */
+			if ((diag>diag_max) && (diag<diag_src - 20))
+			{
+				diag_max = diag;
+				border = vertices;
+			}
+		}
+	}
+	line(src, border[0], border[1], cvScalar(255, 0, 0), 4);
+	line(src, border[1], border[2], cvScalar(0, 255, 0), 4);
+	line(src, border[2], border[3], cvScalar(0, 255, 0), 4);
+	line(src, border[3], border[0], cvScalar(0, 255, 255), 4);
+	/*namedWindow("DetectBorder", WINDOW_NORMAL);
+	while (waitKey(50) != 13)
+	{
+
+		imshow("DetectBorder", src);
+	}
+	destroyWindow("DetectBorder");*/
+	/* Transform border */
+	Point2f inputQuad[4], outputQuad[4];
+	for (size_t i = 0; i < 4; i++)
+	{
+		inputQuad[i] = Point2f(border[i]);
+		/* Get pixel location of 4 corners*/
+		if (inputQuad[i].x > src.cols / 2) outputQuad[i].x = src.cols;
+		else outputQuad[i].x = 0;
+		if (inputQuad[i].y > src.rows / 2) outputQuad[i].y = src.rows;
+		else outputQuad[i].y = 0;
+	}
+	bool Check_b = false;
+	for (size_t i = 0; i < 4; i++)
+	{
+		for (size_t j = 0; j < 4; j++)
+		{
+			if (i != j)
+			{
+				/* Check if picking duplicated corners */
+				if ((outputQuad[i].x == outputQuad[j].x) &&  \
+				    (outputQuad[i].y == outputQuad[j].y))
+					 Check_b=true;
+			}
+		}
+	}
+	if (!Check_b)
+	{
+		M = getPerspectiveTransform(inputQuad, outputQuad);
+		warpPerspective(src, board, M, board.size());
+	}
+}
+
+/* Detect number of squares in board, in each col, in each row, squares's width and squares's height */
+void DetectSquares() 
+{
+
+	int x, y;
+ 	cvtColor(board, board_g, CV_BGR2GRAY);
+	threshold(board_g, board_b, Threshold, 255, CV_THRESH_BINARY);
+	namedWindow("DetectBo", WINDOW_NORMAL);
+	imshow("DetectBo", board_g);
+	findContours(board_b, contours, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
+	for (size_t i = 0; i < contours.size(); i++)
+	{
+		approxPolyDP(contours[i], vertices, arcLength(contours[i], 1)*0.02, 1);
+		if (vertices.size() == 4)
+		{
+			x = abs(vertices[0].x - vertices[2].x);
+			y = abs(vertices[0].y - vertices[2].y);
+			if ((x>5) && (y > 5) && (x<board.cols - 10) && (y<board.rows - 10))
+			{
+				squares[square_count] = vertices;
+				square_count++;
+				sum_x += x;
+				sum_y += y;
+				line(board, vertices[0], vertices[1], cvScalar(255, 255, 0), 4);
+				line(board, vertices[1], vertices[2], cvScalar(0, 255, 255), 4);
+				line(board, vertices[2], vertices[3], cvScalar(0, 255, 255), 4);
+				line(board, vertices[3], vertices[0], cvScalar(100, 255, 255), 4);
+			}
+
+		}
+	}
+	square_w = (float)(sum_y + calibLine) / square_count;
+	square_h = (float)(sum_x + calibLine) / square_count;
+	board_rows = board.rows / square_w;
+	board_cols = board.cols / square_h;
+	namedWindow("DetectSquares", WINDOW_NORMAL);
+	while (waitKey(50) != 13)
+	{
+
+		imshow("DetectSquares", board);
+	}
+	destroyWindow("DetectSquares");
+}
+
+void SetupDraw()
+{
+	drawX = imread("X.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+	drawO = imread("O.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+	namedWindow("Draw", WINDOW_AUTOSIZE);
+	createTrackbar("Calib line", "Draw", &calibLine, 500);
+	createTrackbar("Level X", "Draw", &level1, 100);
+	createTrackbar("Level O", "Draw", &level2, 300);
+
+	while (waitKey(20) != 13) // If OK -> Enter
+	{
+		board_rows = board.rows / (square_w + calibLine);
+		board_cols = board.cols / (square_h + calibLine);
+		draw = Mat::ones(board_rows * 50, board_cols * 50, CV_8U)*255.;
+		for (int i = 1; i < board_cols; i++)
+			line(draw, Point2f(i*(draw.cols / board_cols), 0),  \
+			           Point2f(i*(draw.cols / board_cols), draw.rows), (0, 0, 0), 5, 8, 0);
+		for (int i = 1; i < board_rows; i++)
+			line(draw, Point2f(0, i*(draw.rows / board_rows)),  \
+			           Point2f(draw.cols, i*(draw.rows / board_rows)), (0, 0, 0), 5, 8, 0);
+		imshow("Draw", draw);
+	}
+
+}
+
+void redrawBoard()
+{
+	cvtColor(src, src_g, CV_BGR2GRAY);
+	threshold(src_g, src_b, Threshold, 255, THRESH_BINARY);
+	draw = Mat::ones(board_rows * 50, board_cols * 50, CV_8U)*255.;
+	for (int i = 1; i < board_cols; i++)
+		line(draw, Point2f(i*(draw.cols / board_cols), 0),  \
+				   Point2f(i*(draw.cols / board_cols), draw.rows), (0, 0, 0), 5, 8, 0);
+	for (int i = 1; i < board_rows; i++)
+		line(draw, Point2f(0, i*(draw.rows / board_rows)),  \
+		           Point2f(draw.cols, i*(draw.rows / board_rows)), (0, 0, 0), 5, 8, 0);
+}
+
+void getBoard()
+{
+	cvtColor(src, src_g, CV_BGR2GRAY);
+	medianBlur(src_g, src_g, 1);
+	threshold(src_g, src_b, Threshold, 255, THRESH_BINARY);
+	DetectBorder();
+	warpPerspective(src, board, M, board.size());
+	namedWindow("Board", CV_WINDOW_NORMAL);
+	imshow("Board", board);
+}
+
+void processSquare(vector<Point> square_corners) // Get the squares from 4 point
+{
+	square_w = board.rows / board_rows;
+	square_h = board.cols / board_cols;
+	float xMed, yMed;
+	// Draw lines on square borders
+	line(board, square_corners[0], square_corners[1], cvScalar(255, 0, 0), 4);
+	line(board, square_corners[1], square_corners[2], cvScalar(0, 255, 0), 4);
+	line(board, square_corners[2], square_corners[3], cvScalar(0, 255, 0), 4);
+	line(board, square_corners[3], square_corners[0], cvScalar(0, 255, 255), 4);
+
+	int xMax = square_corners[2].x, xMin = square_corners[0].x;
+	int yMax = square_corners[2].y, yMin = square_corners[0].y;
+	if (square_corners[0].x > square_corners[2].x)
+	{
+		xMax = square_corners[0].x;
+		xMin = square_corners[2].x;
+	}
+	if (square_corners[0].y > square_corners[2].y)
+	{
+		yMax = square_corners[0].y;
+		yMin = square_corners[2].y;
+	}
+	// Extract square from board
+	Mat temp = board(Range(yMin, yMax), Range(xMin, xMax));
+	cvtColor(temp, temp, CV_BGR2GRAY);
+	// Square center
+	xMed = (float)(xMax + xMin) / 2;
+	yMed = (float)(yMax + yMin) / 2;
+	if ((yMed / square_w  < board_rows) && (xMed / square_h < board_cols))
+	{
+		Mat temp_draw = draw(Range(floor(yMed/square_w)*50, (floor(yMed/square_w)+1)*50),   \
+		                     Range(floor(xMed/square_h)*50, (floor(xMed/square_h)+1)*50));
+		flag = findXO(temp);
+		if (flag == 1) bitwise_and(temp_draw, drawX, temp_draw);
+		else if (flag == 0) bitwise_and(temp_draw, drawO, temp_draw);
+	}
+}
+
+int findXO(Mat img)
+{	
+	int rec = -1;
+	float t = (float)findblack(img) / (img.cols*img.rows) - (float)level1/1000;
+	if (t>0)
+	{
+		int check = 0;
+		
+		Mat modelx = imread("xmask1.jpg", CV_8UC1);
+		Mat modelo = imread("omask.jpg", CV_8UC1);
+		resize(modelx, modelx, Size(img.cols, img.rows), 1, 1, INTER_LINEAR);
+		resize(modelo, modelo, Size(img.cols, img.rows), 1, 1, INTER_LINEAR);
+
+		//float initx = findblack(modelx);
+		float init = findblack(img);
+
+		modelx = modelx + img;
+		modelo = modelo + img;
+		float kx = findblack(modelx) / init;
+		float ko = findblack(modelo) / init;
+		if (kx > ko)  rec = 1;
+		else if (kx < ko) rec = 0;
+	}
+	else rec = -1;
+	return rec;
+}
+
+float findblack(Mat img)
+{
+
+	float count = 0;
+	for (int i = 0; i < img.rows; i++)
+	{
+		for (int j = 0; j < img.cols; j++)
+		{
+			if (img.at<uchar>(i, j) < Threshold) count++;
+		}
+	}
+	return count;
+}
+
+int Diagonal(vector<Point> rect) /* Find the biggest diagonal */
+{
+	int Diagonal_1 = sqrt((rect[0].x - rect[2].x)*(rect[0].x - rect[2].x) + \
+						  (rect[0].y - rect[2].y)*(rect[0].y - rect[2].y));
+	int Diagonal_2 = sqrt((rect[1].x - rect[3].x)*(rect[1].x - rect[3].x) + \
+						  (rect[1].y - rect[3].y)*(rect[1].y - rect[3].y));
+	if (Diagonal_1 > Diagonal_2) return Diagonal_1;
+	else return Diagonal_2;
+}
+
